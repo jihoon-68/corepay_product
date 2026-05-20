20035b93fa50b4218cf0bff3cd447415ff3c80d4
jihoon-68 <pkrwlgns64@gmail.com>
2026-05-21 01:35:37 +0900

    feat: ProductEventHandler 수정

--- src/main/java/org/example/corepayproductservice/product/application/event/ProductEventHandler.java
+++ src/main/java/org/example/corepayproductservice/product/application/event/ProductEventHandler.java
@@ -6,6 +6,7 @@ import org.example.corepayproductservice.product.infrastructure.kafka.ProductEve
 import org.example.corepayproductservice.product.infrastructure.kafka.event.OrderCancelEvent;
 import org.example.corepayproductservice.product.infrastructure.kafka.event.ProductCreatedEvent;
 import org.example.corepayproductservice.product.infrastructure.kafka.event.StockDecrementedEvent;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -18,16 +19,19 @@ public class ProductEventHandler {
     private final ProductEventProducer producer;
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Async
     public void ProductCreatedEvent(ProductCreatedEvent event){
         producer.sendProductCreatedEvent(event);
     }
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Async
     public void StockDecrementedEvent(StockDecrementedEvent event){
         producer.sendStockDecrementedEvent(event);
     }
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Async
     public void OrderCancelEvent(OrderCancelEvent event){
         producer.sendOrderCancelEvent(event);
     }
